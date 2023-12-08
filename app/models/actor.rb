93492f9739b4174522fc61ad44020cdc0238f11c
sindhu1s <146214337+sindhu1s@users.noreply.github.com>
2023-12-08 07:28:55 +0000

    msm validations

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -11,7 +11,26 @@
 #  updated_at :datetime         not null
 #
 class Actor < ApplicationRecord
+  validates(:name, presence: true)
+
   def characters
-    return Character.where({ :actor_id => self.id })
+    key = self.id
+
+    the_many = Character.where({ :actor_id => key })
+
+    return the_many
+  end
+
+  def filmography
+    the_many = Array.new
+
+    self.characters.each do |joining_record|
+      destination_record = joining_record.movie
+
+      the_many.push(destination_record)
+    end
+
+    return the_many
   end
 end
+
